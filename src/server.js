8890d8ae6b0c4bba44031386144cfa188830793f
shabazzramadan14-sudo <shabazzramadan14@gmail.com>
2026-04-08 22:34:24 -0500

    Add backend server.js with Express, Socket.io, and MongoDB setup

---
+++ src/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const mongoose = require('mongoose');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+// MongoDB connection
+const mongoURI = 'your_mongodb_connection_string_here';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.log(err));
+
+// Socket.io connection
+io.on('connection', (socket) => {
+    console.log('New client connected');
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
+});
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
